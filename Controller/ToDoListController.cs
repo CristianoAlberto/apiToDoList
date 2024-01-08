a1d18d835aec00b7cf51f836d31761169726df97
CristianoAlberto <cristianoalberto908347@gmail.com>
2024-01-08 17:03:40 +0100

    feat: algumas configuraçoes adcionadas

--- Controller/ToDoListController.cs
+++ Controller/ToDoListController.cs
@@ -1,8 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 
-namespace toDoList;
-
-public class ToDoListController
+namespace toDoList
 {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoListController : ControllerBase
+    {
+        private readonly IToDoListRepository _toDoListRepository;
 
+        public ToDoListController(IToDoListRepository toDoListRepository)
+        {
+            _toDoListRepository = toDoListRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ToDoListModel>>> GetTaskAsync()
+        {
+            List<ToDoListModel> tasks = await _toDoListRepository.GetAllAsync();
+            return Ok(tasks);
+        }
+
+    }
 }
+
